0e5c1bb3d6027a6c57baa67805a58cb1205acb67
Pete Maynard <pgm987@gmail.com>
2016-06-22 16:59:29 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,31 @@
+import vobject
+from jinja2 import Template
+from os import listdir
+from os.path import isfile, join
+
+cal_location = "/home/osaka/.calendars/work.ics/"
+template_location = "./index.template"
+html_location = "./public_html/index.html"
+
+events = [f for f in listdir(cal_location) if isfile(join(cal_location, f)) and f.endswith('.ics')]
+
+cal_events = []
+
+for e in events: 
+	try: 
+		cal = vobject.readOne(open(join(cal_location, e), 'rb').read())
+		cal_events.append(cal.vevent)
+	except:
+		print "> Unable to read: ", e, "\n"
+
+events_data = ""
+for event in cal_events:
+	events_data += "{{\n title : \"{0}\",\n start : '{1}',\n end : '{2}'\n}},\n".format(event.getChildValue('summary'), event.getChildValue('dtstart'), event.getChildValue('dtend'))
+
+events_data = "[" + events_data[:-2] + "]"
+
+
+template = Template(open(template_location, 'rb').read())
+output = open(html_location, 'w')
+output.write(template.render(events_data=events_data))
+
